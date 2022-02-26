28956e885f7c245a53361d93f99786fe87d7d36a
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2022-02-26 15:06:59 +0100

    app fix

---
+++ src/data/dataArrays.js
@@ -0,0 +1,507 @@
+export const categories = [
+  {
+    id: 3,
+    name: 'neurologist',
+    photo_url:
+    'https://cdn.discordapp.com/attachments/933742466438410342/946502917156929626/brain.png'
+  },
+  {
+    id: 1,
+    name: 'cardiologist',
+    photo_url: 'https://cdn.discordapp.com/attachments/933742466438410342/946502918142586900/heart.png'
+  },
+  {
+    id: 2,
+    name: 'dermatologist',
+    photo_url:
+      'https://cdn.discordapp.com/attachments/933742466438410342/946502917718937680/dermis.png'
+  },
+  {
+    id: 4,
+    name: 'nephrologist',
+    photo_url:
+    'https://cdn.discordapp.com/attachments/933742466438410342/946502918318727239/kidney.png'
+  },
+  {
+    id: 0,
+    name: 'general preactitioner',
+    photo_url: 'https://media.discordapp.net/attachments/933742466438410342/946502917345669170/cardiology.png?width=468&height=468'
+  },
+  {
+    id: 5,
+    name: 'orthopedist',
+    photo_url: 'https://cdn.discordapp.com/attachments/933742466438410342/946502969531179009/orthopedics_1.png'
+  },
+  {
+    id: 6,
+    name: 'gynecologist',
+    photo_url: 'https://cdn.discordapp.com/attachments/933742466438410342/946502970198069338/sexual-health.png'
+  },
+  {
+    id: 7,
+    name: 'diabetologist',
+    photo_url: 'https://cdn.discordapp.com/attachments/933742466438410342/946502970487484456/sugar-blood-level.png'
+  },
+  {
+    id: 8,
+    name: 'dentist',
+    photo_url: 'https://cdn.discordapp.com/attachments/933742466438410342/946502970827218974/tooth.png'
+  },
+
+  
+];
+
+export const recipes = [
+  {
+    recipeId: 122,
+    categoryId: 3,
+    title: 'dr mortadha',
+    photo_url: 'https://th.bing.com/th/id/OIP.pz2e9Dmjs8EOPvk90hqH0QHaKF?pid=ImgDet&rs=1',
+    photosArray: [
+      'https://th.bing.com/th/id/OIP.pz2e9Dmjs8EOPvk90hqH0QHaKF?pid=ImgDet&rs=1',
+      "https://namelymarly.com/wp-content/uploads/2018/04/20180415_Beet_Lasagna_10.jpg",
+      'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width600/img/news_impact/photo/burger-fijpg-57e7e5907630c2ad.jpg',
+      'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
+      'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
+    ],
+    time: '35',
+    ingredients: [[0, '200ml'], [1, '5g'], [2, '300g']],
+    description:
+    'mortadha tbib '
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Dr Oussama',
+    photo_url:
+      'https://i.pinimg.com/originals/f5/ab/d9/f5abd966ee9915350800bfee990a15fb.jpg',
+    photosArray: [
+      'https://i.pinimg.com/originals/f5/ab/d9/f5abd966ee9915350800bfee990a15fb.jpg',
+      'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
+      'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
+    ],
+    time: '35',
+    ingredients: [
+      [59, '1'],
+      [60, '1/2 lbs'],
+      [61, '1/2 liters'],
+    ],
+    description: ' tbib tbibi'
+  },
+  {
+    recipeId: 2,
+    categoryId: 3,
+    title: 'DR Badis',
+    photo_url: 'https://fr.web.img4.acsta.net/newsv7/19/10/30/15/08/4871429.jpg',
+    photosArray: [
+      'https://fr.web.img4.acsta.net/newsv7/19/10/30/15/08/4871429.jpg',
+      'https://ichef.bbci.co.uk/news/660/cpsprodpb/B2C0/production/_106106754_vegnuggets976.jpg',
+      'https://pixel.nymag.com/imgs/daily/grub/2017/11/22/22-mcds-chicken-tenders.w330.h330.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-img.health.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Flarge_16_9%2Fpublic%2Fstyles%2Fmain%2Fpublic%2Fgettyimages-508510211.jpg%3Fitok%3Dh-Uryi8r&w=400&c=sc&poi=face&q=85'
+    ],
+    time: '35',
+    ingredients: [
+      [0, '2 quarts'],
+      [16, '1'],
+      [12, '1 cup'],
+      [18, '1 cup'],
+      [19, '1 teaspoon'],
+      [1, '2 teaspoons'],
+      [4, '1/4 teaspoons'],
+      [7, '1/8 teaspoons'],
+      [20, '1/2 teaspoons'],
+      [21, '4']
+    ],
+    description:
+    'tbib tayra '
+  },
+  {
+    recipeId: 3,
+    categoryId: 3,
+    title: 'Dr Amine',
+    photo_url:
+      'https://th.bing.com/th/id/R.70a8b012b68062da96dfdde289f28ce7?rik=AbLct6L99xHmjQ&riu=http%3a%2f%2fwww.healthconsultantsnj.com%2fwp-content%2fuploads%2fdr-omar-nabulsi.jpg&ehk=dJpj8Yg0O4pJhCvSQuZ2mJ%2fZmOVtbdhTeb4kUHJD0Dg%3d&risl=&pid=ImgRaw&r=0',
+    photosArray: [
+      'https://th.bing.com/th/id/R.70a8b012b68062da96dfdde289f28ce7?rik=AbLct6L99xHmjQ&riu=http%3a%2f%2fwww.healthconsultantsnj.com%2fwp-content%2fuploads%2fdr-omar-nabulsi.jpg&ehk=dJpj8Yg0O4pJhCvSQuZ2mJ%2fZmOVtbdhTeb4kUHJD0Dg%3d&risl=&pid=ImgRaw&r=0',
+      'https://cdn.junglecreations.com/wp/junglecms/2018/07/4164c5bd-wide-thumbnail.jpg',
+      'https://pinchofyum.com/wp-content/uploads/Crunchwrap-Inside.jpg',
+      'https://monsonmadethis.com/wp-content/uploads/2017/10/IMG_20171015_161017_025-e1533869302263.jpg'
+    ],
+    time: '35',
+    ingredients: [
+      [0, '2 tablespoons'],
+      [22, '1/2'],
+      [23, '2 tablespoons'],
+      [7, '2 cloves'],
+      [3, '1 teaspoon'],
+      [24, '1 tablespoon'],
+      [25, '1 lb'],
+      [1, '2 teaspoons'],
+      [4, '2 teaspoons'],
+      [26, '15 oz'],
+      [27, '8'],
+      [28, '2'],
+      [29, '1 cup']
+    ],
+    description:
+'ydaw5ek oumour teb wfazet yhbalek'  },
+// 
+];
+export const ingredients = [
+  {
+    ingredientId: 0,
+    name: 'Oil',
+    photo_url: 'https://ak7.picdn.net/shutterstock/videos/27252067/thumb/11.jpg'
+  },
+  {
+    ingredientId: 1,
+    name: 'Salt',
+    photo_url:
+      'https://image.freepik.com/free-photo/sea-salt-wooden-bowl-isolated-white-background_29402-416.jpg'
+  },
+  {
+    ingredientId: 2,
+    name: 'Russet potatoes',
+    photo_url: 'http://www.valleyspuds.com/wp-content/uploads/Russet-Potatoes-cut.jpg'
+  },
+  {
+    ingredientId: 3,
+    name: 'Paprika',
+    photo_url:
+      'https://image.freepik.com/free-photo/red-chilli-pepper-powder-isolated-white-background_55610-28.jpg'
+  },
+  {
+    ingredientId: 4,
+    name: 'Black Pepper',
+    photo_url: 'https://ak0.picdn.net/shutterstock/videos/26741680/thumb/1.jpg'
+  },
+  {
+    ingredientId: 5,
+    name: 'Celery salt',
+    photo_url: 'https://www.hasiroglugurme.com/images/urunler/Koftelik-Esmer-Bulgur-resim-297.jpg'
+  },
+  {
+    ingredientId: 6,
+    name: 'Dried sage',
+    photo_url:
+      'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Esxjvv7/super-slow-motion-dried-sage-falling-on-white-background_n1xg2gxzg__F0000.png'
+  },
+  {
+    ingredientId: 7,
+    name: 'Garlic powder',
+    photo_url:
+      'https://us.123rf.com/450wm/belchonock/belchonock1808/belchonock180818180/106007144-bowl-of-dry-garlic-powder-on-white-background.jpg?ver=6'
+  },
+  {
+    ingredientId: 8,
+    name: 'Ground allspice',
+    photo_url:
+      'https://www.savoryspiceshop.com/content/mercury_modules/cart/items/2/6/9/2695/allspice-berries-jamaican-ground-1.jpg'
+  },
+  {
+    ingredientId: 9,
+    name: 'Dried oregano',
+    photo_url: 'https://frutascharito.es/886-large_default/oregano.jpg'
+  },
+  {
+    ingredientId: 10,
+    name: 'Dried basil',
+    photo_url: 'https://www.honeychop.com/wp-content/uploads/2015/09/Dried-Mint.png'
+  },
+  {
+    ingredientId: 11,
+    name: 'Dried marjoram',
+    photo_url: 'https://images-na.ssl-images-amazon.com/images/I/71YATIBqBYL._SX355_.jpg'
+  },
+  {
+    ingredientId: 12,
+    name: 'All-purpose flour',
+    photo_url:
+      'https://images.assetsdelivery.com/compings_v2/seregam/seregam1309/seregam130900036.jpg'
+  },
+  {
+    ingredientId: 13,
+    name: 'Brown sugar',
+    photo_url:
+      'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/BALQTtekliuc6iu4u/rotating-brown-sugar-in-a-white-container-on-white-background_sis0xtbyl_thumbnail-full01.png'
+  },
+  {
+    ingredientId: 14,
+    name: 'Kosher salt',
+    photo_url:
+      'https://d1yn1kh78jj1rr.cloudfront.net/image/preview/r64-6MxPQjlatyfjp/storyblocks-top-view-of-ceramic-salt-cellar-with-coarse-grained-sea-salt-isolated-on-white-background_SPzKionPuV_SB_PM.jpg'
+  },
+  {
+    ingredientId: 15,
+    name: 'Whole chicken',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/two-raw-chicken-drumsticks-isolated-260nw-632125991.jpg'
+  },
+  {
+    ingredientId: 16,
+    name: 'Eggs',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/egg-whites-yolk-cup-isolated-260nw-1072453787.jpg'
+  },
+  {
+    ingredientId: 17,
+    name: 'Quarts neutral oil',
+    photo_url:
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fgettyimages-464433694_0.jpg%3Fitok%3DK42YR2GV&w=400&c=sc&poi=face&q=85'
+  },
+  {
+    ingredientId: 18,
+    name: 'Water',
+    photo_url: 'https://ak1.picdn.net/shutterstock/videos/829561/thumb/11.jpg'
+  },
+  {
+    ingredientId: 19,
+    name: 'Onion Powder',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/mixed-spices-isolated-on-white-260nw-662383828.jpg'
+  },
+  {
+    ingredientId: 20,
+    name: 'MSG',
+    photo_url:
+      'https://img.freepik.com/free-photo/monosodium-glutamate-wood-spoon-white-background_55883-399.jpg?size=626&ext=jpg'
+  },
+  {
+    ingredientId: 21,
+    name: 'Chicken Breast',
+    photo_url:
+      'https://us.123rf.com/450wm/utima/utima1602/utima160200063/53405187-raw-chicken-breast-fillets.jpg?ver=6'
+  },
+  {
+    ingredientId: 22,
+    name: 'Onion chopped',
+    photo_url: 'https://s3.envato.com/files/246703499/IMG_1752_5.jpg'
+  },
+  {
+    ingredientId: 23,
+    name: 'Tomato paste',
+    photo_url:
+      'http://d3e1m60ptf1oym.cloudfront.net/45bab59a-363c-11e1-ab4e-bf4c2e0bb026/PANELA_xgaplus.jpg'
+  },
+  {
+    ingredientId: 24,
+    name: 'Chilli Powder',
+    photo_url:
+      'https://us.123rf.com/450wm/nuttapong/nuttapong1505/nuttapong150500009/40458002-paprika-powder-isolated-on-white-background.jpg?ver=6'
+  },
+  {
+    ingredientId: 25,
+    name: 'Ground Beef',
+    photo_url:
+      'https://images.radio.com/kmoxam/s3fs-public/styles/nts_image_cover_tall_775x425/public/dreamstime_s_39607998.jpg?XCM.w1UGOp9sVKkWGQZe7_JIsRddxoIK&itok=3M6KcFLH&c=73fb6497175b4c1a5c79e3ede816656a'
+  },
+  {
+    ingredientId: 26,
+    name: 'Can kidney beans, rinsed and drained ',
+    photo_url:
+      'https://www.seriouseats.com/images/2014/04/20140414-pile-of-beans-primary-1500x1125.jpg'
+  },
+  {
+    ingredientId: 27,
+    name: 'Large Tortillas',
+    photo_url: 'https://upload.wikimedia.org/wikipedia/commons/5/56/NCI_flour_tortillas.jpg'
+  },
+  {
+    ingredientId: 28,
+    name: 'Firtos',
+    photo_url:
+      'https://previews.123rf.com/images/ksena32/ksena321510/ksena32151000090/45863494-fried-fish-on-a-white-background.jpg'
+  },
+  {
+    ingredientId: 29,
+    name: 'Shredded cheddar',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/top-view-small-bowl-filled-260nw-284460308.jpg'
+  },
+  {
+    ingredientId: 30,
+    name: 'Lime',
+    photo_url: 'https://ak8.picdn.net/shutterstock/videos/23271748/thumb/1.jpg'
+  },
+
+  {
+    ingredientId: 31,
+    name: 'Ground cumin',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/pile-cumin-powder-isolated-on-260nw-1193262853.jpg'
+  },
+  {
+    ingredientId: 32,
+    name: 'Cayenne pepper',
+    photo_url: 'https://ak7.picdn.net/shutterstock/videos/11461337/thumb/1.jpg'
+  },
+  {
+    ingredientId: 33,
+    name: 'Flaky white fish',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/roach-river-fish-isolated-on-260nw-277764143.jpg'
+  },
+  {
+    ingredientId: 34,
+    name: 'Avocado',
+    photo_url:
+      'https://www.redwallpapers.com/public/redwallpapers-large-thumb/avocado-cut-stone-leaves-white-background-free-stock-photos-images-hd-wallpaper.jpg'
+  },
+  {
+    ingredientId: 35,
+    name: 'Red Pepper Flakes',
+    photo_url:
+      'https://as1.ftcdn.net/jpg/02/06/55/10/500_F_206551074_mVczUrAWOSMaw8kR48FQDQBqDw47jCtL.jpg'
+  },
+  {
+    ingredientId: 36,
+    name: 'Onions',
+    photo_url: 'http://www.allwhitebackground.com/images/2/2650.jpg'
+  },
+  {
+    ingredientId: 37,
+    name: 'Green Pepper',
+    photo_url: 'https://ak9.picdn.net/shutterstock/videos/4055509/thumb/1.jpg'
+  },
+  {
+    ingredientId: 38,
+    name: 'Red Pepper',
+    photo_url: 'https://ak9.picdn.net/shutterstock/videos/10314179/thumb/1.jpg'
+  },
+  {
+    ingredientId: 39,
+    name: 'Pizza dough',
+    photo_url:
+      'https://image.shutterstock.com/image-photo/fresh-raw-dough-pizza-bread-260nw-518950903.jpg'
+  },
+  {
+    ingredientId: 40,
+    name: 'Ketchup sauce',
+    photo_url:
+      'https://st2.depositphotos.com/5262887/11050/i/950/depositphotos_110501208-stock-photo-ketchup-bowl-isolated-on-white.jpg'
+  },
+  {
+    ingredientId: 41,
+    name: 'Hot Sauce',
+    photo_url:
+      'https://media.istockphoto.com/photos/opened-can-of-spaghetti-sauce-on-a-white-background-picture-id497704752?k=6&m=497704752&s=612x612&w=0&h=JnL54buYu1Z3fGtd8uNdjFxiAKwlxoDluD6jbIfSaZI='
+  },
+  {
+    ingredientId: 42,
+    name: 'Butter',
+    photo_url: 'https://redrockstoffee.com/media/2016/11/AdobeStock_76417550.jpeg'
+  },
+  {
+    ingredientId: 43,
+    name: 'Heavy Cream',
+    photo_url:
+      'https://media.istockphoto.com/photos/mayonnaise-in-bowl-isolated-on-white-background-picture-id614981116?k=6&m=614981116&s=612x612&w=0&h=LtbsI2HQXOTERYuP9YJ2PJfRF3W6DcyZ798fxMcQWC0='
+  },
+  {
+    ingredientId: 44,
+    name: 'whole-milk plain yogurt',
+    photo_url:
+      'https://st.depositphotos.com/2757384/3317/i/950/depositphotos_33170129-stock-photo-pouring-a-glass-of-milk.jpg'
+  },
+  {
+    ingredientId: 45,
+    name: 'Chesse',
+    photo_url: 'https://ak7.picdn.net/shutterstock/videos/3619997/thumb/1.jpg'
+  },
+  {
+    ingredientId: 46,
+    name: 'Mozzarella',
+    photo_url:
+      'https://t3.ftcdn.net/jpg/02/06/73/98/500_F_206739841_suPu6qDPHlowFqx9qo8fLqV8sNevL2g3.jpg'
+  },
+  {
+    ingredientId: 47,
+    name: 'celery stalks',
+    photo_url:
+      'https://cdn4.eyeem.com/thumb/6d1b3957c7caa9b73c3e0f820ef854b931808139-1538043742765/w/750'
+  },
+  {
+    ingredientId: 48,
+    name: 'Parmesan Chesse',
+    photo_url: 'https://ak7.picdn.net/shutterstock/videos/3721877/thumb/1.jpg'
+  },
+  {
+    ingredientId: 49,
+    name: 'pancetta',
+    photo_url:
+      'https://previews.123rf.com/images/onlyfabrizio/onlyfabrizio1606/onlyfabrizio160600002/60198502-raw-stripes-of-pancetta-stesa-on-a-white-background.jpg'
+  },
+  {
+    ingredientId: 50,
+    name: 'Spaghetti',
+    photo_url:
+      'https://previews.123rf.com/images/mfron/mfron1204/mfron120400098/13306773-bunch-of-spaghetti-nudeln-isoliert-auf-wei%C3%9Fem-hintergrund.jpg'
+  },
+  {
+    ingredientId: 51,
+    name: 'Garlic',
+    photo_url: 'https://image.freepik.com/free-photo/fresh-garlic-white-background_1339-17012.jpg'
+  },
+  {
+    ingredientId: 52,
+    name: 'Lasagna noodles',
+    photo_url:
+      'https://previews.123rf.com/images/velkol/velkol1110/velkol111000004/11083085-an-image-of-raw-lasagna-on-white-background.jpg'
+  },
+  {
+    ingredientId: 53,
+    name: 'Italian sauce',
+    photo_url:
+      'https://previews.123rf.com/images/arinahabich/arinahabich1504/arinahabich150400858/38827029-raw-italian-sausage-on-a-white-background-.jpg'
+  },
+  {
+    ingredientId: 54,
+    name: 'Crushed Tomatoes',
+    photo_url:
+      'https://previews.123rf.com/images/merkulovnik/merkulovnik1406/merkulovnik140600100/28751626-crushed-tomato-isolated-on-white-background.jpg'
+  },
+  {
+    ingredientId: 55,
+    name: 'Sugar',
+    photo_url:
+      'https://previews.123rf.com/images/sommai/sommai1411/sommai141100034/33199985-sugar-cubes-in-a-bowl-isolated-on-white-background.jpg'
+  },
+  {
+    ingredientId: 56,
+    name: 'minced fresh parsley',
+    photo_url:
+      'https://t4.ftcdn.net/jpg/02/15/78/05/240_F_215780551_Eid0xpP1M2fokvuEcvJj8uqhROLJkb3p.jpg'
+  },
+  {
+    ingredientId: 57,
+    name: 'ricotta cheese',
+    photo_url:
+      'https://previews.123rf.com/images/barkstudio/barkstudio1608/barkstudio160800351/61418602-ricotta-cheese-into-a-bowl-in-white-background.jpg'
+  },
+  {
+    ingredientId: 58,
+    name: ' fennel seed',
+    photo_url:
+      'https://previews.123rf.com/images/pinkomelet/pinkomelet1710/pinkomelet171000227/88851299-close-up-the-fennel-seed-on-white-background.jpg'
+  },
+  {
+    ingredientId: 59,
+    name: 'Banana',
+    photo_url:
+      'https://www.conservationmagazine.org/wp-content/uploads/2013/04/sterile-banana.jpg'
+  },
+  {
+    ingredientId: 60,
+    name: 'Frozen Straberries',
+    photo_url:
+      'https://www.cascadianfarm.com/wp-content/uploads/2018/12/Strawberries_Main_0218.png'
+  },
+  {
+    ingredientId: 61,
+    name: 'Greek Yogurt',
+    photo_url:
+      'http://images.media-allrecipes.com/userphotos/960x960/3758635.jpg'
+  },
+];
+
---
+++ src/screens/Authentication/LoginScreen.js
@@ -0,0 +1,292 @@
+import React, { useState, useEffect } from 'react';
+import styles from './styles'
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Icon } from 'react-native-elements';
+import { useFonts } from 'expo-font';
+import NSLight from '../../../assets/fonts/NunitoSans-Light.ttf';
+import NSRegular from '../../../assets/fonts/NunitoSans-Regular.ttf';
+import NSBold from '../../../assets/fonts/NunitoSans-Bold.ttf';
+import NSExtraBold from '../../../assets/fonts/NunitoSans-ExtraBold.ttf';
+
+export default function LoginScreen3({navigation}) {
+  const [loaded] = useFonts({
+    NSLight,
+    NSRegular,
+    NSBold,
+    NSExtraBold,
+  });
+  const presslogin=()=>{
+    navigation.navigate('Home')
+  }
+
+  const [activeTab, setActiveTab] = useState('Login');
+
+  useEffect(function () {
+    StatusBar.setBarStyle('light-content', true);
+  }, []);
+
+  function switchTab() {
+    if (activeTab === 'Login') {
+      setActiveTab('Register');
+    } else {
+      setActiveTab('Login');
+    }
+  }
+  
+ function Login(props) {
+    const [showLoginPassword, setShowLoginPassword] = useState(false);
+   
+
+    return (
+      <View style={{ marginTop: 10 }}>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4 }}
+            name='envelope'
+            type='font-awesome'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='email-address'
+            textContentType='emailAddress'
+            // autoCapitalize={false}
+            autoCompleteType='email'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4 }}
+            name='key'
+            type='font-awesome-5'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Password'
+            placeholderTextColor='#f1f2f6'
+            secureTextEntry={!showLoginPassword}
+            textContentType='password'
+            returnKeyType='done'
+          />
+          <TouchableOpacity
+            style={{ paddingVertical: 4 }}
+            onPress={() => {
+              setShowLoginPassword(!showLoginPassword);
+            }}
+          >
+            <Icon
+              style={{ paddingHorizontal: 4 }}
+              name='eye'
+              type='font-awesome'
+              color='#74b3ce'
+              size={22}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button}  >
+          <Text style={styles.buttonText} onPress={presslogin}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <View style={styles.socialLoginView}>
+          <TouchableOpacity style={styles.socialLoginTouchable}>
+            <Icon name='google' type='font-awesome' color='#F16529' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.socialLoginTouchable}>
+            <Icon name='facebook' type='font-awesome' color='#F16529' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.socialLoginTouchable}>
+            <Icon name='apple' type='font-awesome' color='#F16529' />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function Register() {
+    const [showRegisterPassword, setShowRegisterPassword] = useState(false);
+    return (
+      <View style={{ marginTop: 10 }}>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='user'
+            type='font-awesome'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Full Name'
+            placeholderTextColor='#f1f2f6'
+            textContentType='name'
+            autoCompleteType='name'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='envelope'
+            type='font-awesome'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='email-address'
+            textContentType='emailAddress'
+            autoCapitalize={false}
+            autoCompleteType='email'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='phone'
+            type='font-awesome'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Phone'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='phone-pad'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='key'
+            type='font-awesome-5'
+            color='#74b3ce'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Password'
+            placeholderTextColor='#f1f2f6'
+            secureTextEntry={!showRegisterPassword}
+            textContentType='password'
+            returnKeyType='done'
+          />
+          <TouchableOpacity
+            style={{ paddingVertical: 4 }}
+            onPress={() => {
+              setShowRegisterPassword(!showRegisterPassword);
+            }}
+          >
+            <Icon
+              style={{ paddingHorizontal: 4 }}
+              name='eye'
+              type='font-awesome'
+              color='#74b3ce'
+              size={22}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+        <View>
+          <Text
+            style={{
+              marginHorizontal: 20,
+              marginTop: 30,
+              fontSize: 18,
+              color: '#74b3ce',
+              fontFamily: 'NSBold',
+            }}
+          >
+            Or Register with
+          </Text>
+          <View
+            style={[
+              styles.socialLoginView,
+              { marginTop: 14, justifyContent: 'flex-start' },
+            ]}
+          >
+            <TouchableOpacity
+              style={[styles.socialLoginTouchable, { marginLeft: 0 }]}
+            >
+              <Icon name='google' type='font-awesome' color='#26619c' />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.socialLoginTouchable}>
+              <Icon name='facebook' type='font-awesome' color='#26619c' />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.socialLoginTouchable}>
+              <Icon name='apple' type='font-awesome' color='#26619c' />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  if (!loaded) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient colors={['white', '#74b3ce']} style={styles.container}>
+        <Text style={styles.welcomeText}>
+          {activeTab === 'Login' ? 'Welcome Back' : 'Register Now'}
+        </Text>
+        <View style={styles.switchTabsView}>
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: activeTab === 'Login' ? 4 : 0,
+              borderBottomColor: '#74b3ce',
+              paddingHorizontal: 4,
+              marginRight: 14,
+            }}
+            onPress={() => switchTab()}
+          >
+            <Text style={styles.switchText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: activeTab === 'Register' ? 4 : 0,
+              borderBottomColor: '#74b3ce',
+              paddingHorizontal: 4,
+              marginRight: 14,
+            }}
+            onPress={() => switchTab()}
+          >
+            <Text style={styles.switchText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        {activeTab === 'Login' ? <Login /> : <Register />}
+      </LinearGradient>
+    </TouchableWithoutFeedback>
+  );
+}
+
