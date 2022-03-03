18bce8315137ce1f228775dfa75951d327b7e825
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2022-03-03 16:13:11 +0100

    working in profile

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -30,99 +30,27 @@ function Photos({ photos }) {
           alignItems: 'flex-start',
         }}
       >
-        {photos.map((photo, index) => (
+        {/* {photos.map((photo, index) => ( */}
           <View>
-            <Image
+            {/* <Image
               style={{ width: imgWidth, height: imgWidth }}
               source={{
                 uri: `https://picsum.photos/200/300?random=${index + 1}`,
               }}
-            />
+            /> */}
+            <Text>3andek rendez vous sa3a  6 </Text>
+           
           </View>
-        ))}
+        {/* ))}
+         */}
       </View>
     </View>
   );
 }
 
-function Albums() {
-  const [albums] = useState([
-    {
-      name: 'Animals',
-      images: [
-        'https://i.picsum.photos/id/1074/367/267.jpg?hmac=2YamGD7W1FNtp9UvAVUDdYUm44xzyHCthHqFl6jVT0M',
-        'https://i.picsum.photos/id/237/367/267.jpg?hmac=9Xp8JrOngpF2E_G3tRKnJMhZu5AX8FimulIG_sLj1xg',
-        'https://i.picsum.photos/id/1084/367/267.jpg?hmac=VaCZRCvuoubMR-S6bXItyxmDVwAaumZU2x1ulWE0faU',
-        'https://i.picsum.photos/id/219/367/267.jpg?hmac=S8RAgXxGj5AUho8KQ0hsjW8bhy1d-WunZNm77FCqC3w',
-      ],
-    },
-    {
-      name: 'Food',
-      images: [
-        'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-      ],
-    },
-  ]);
-  const imgWidth = Dimensions.get('screen').width * 0.33333;
-  return (
-    <View style={{ flex: 1, backgroundColor: '#fff', paddingBottom: 20 }}>
-      {albums.map((album) => (
-        <TouchableOpacity style={{ flexDirection: 'row', marginTop: 10 }}>
-          {/* {album.images.map((img) => (
-            <Image
-              style={{ width: imgWidth + 50, height: imgWidth + 50 }}
-              source={{ uri: img }}
-            />
-          ))}
-          <View
-            style={{
-              position: 'absolute',
-              bottom: 10,
-              left: 10,
-              backgroundColor: '#111',
-              paddingHorizontal: 10,
-              paddingVertical: 4,
-              borderRadius: 6,
-            }}
-          >
-            <Text style={{ color: '#fff', fontFamily: 'SSBold', fontSize: 20 }}>
-              {album.name}
-            </Text>
-          </View> */}
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-}
 
-function Tags({ photos }) {
-  const imgWidth = Dimensions.get('screen').width * 0.33333;
-  return (
-    <View style={{}}>
-      {/* <View
-        style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          alignItems: 'flex-start',
-        }}
-      >
-        {photos.map((photo, index) => (
-          <View>
-            <Image
-              style={{ width: imgWidth, height: imgWidth }}
-              source={{
-                uri: `https://picsum.photos/200/300?random=${index + 100}`,
-              }}
-            />
-          </View>
-        ))}
-      </View> */}
-    </View>
-  );
-}
+
+
 
 export default function ProfileScreen1() {
   const [loaded] = useFonts({
@@ -183,32 +111,11 @@ export default function ProfileScreen1() {
                 >
                   <Text style={styles.showContentButtonText}>INFOS</Text>
                 </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    ...styles.showContentButton,
-                    borderBottomWidth: showContent === 'Albums' ? 2 : 0,
-                  }}
-                  onPress={() => setShowContent('Albums')}
-                >
-                  <Text style={styles.showContentButtonText}>APPOINTMENT</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    ...styles.showContentButton,
-                    borderBottomWidth: showContent === 'Tags' ? 2 : 0,
-                  }}
-                  onPress={() => setShowContent('Tags')}
-                >
-                  <Text style={styles.showContentButtonText}>Privacy</Text>
-                </TouchableOpacity>
+ 
               </View>
               {showContent === 'Photos' ? (
                 <Photos photos={new Array(13).fill(1)} />
-              ) : showContent === 'Albums' ? (
-                <Albums />
-              ) : (
-                <Tags photos={new Array(23).fill(1)} />
-              )}
+              ) :null}
             </View>
           </View>
         </>
