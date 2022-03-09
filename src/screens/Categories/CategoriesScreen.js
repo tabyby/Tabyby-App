0c696df284821edaba39a47228592332698734e1
oussama hmaied <ouss.hmaied.14@gmail.com>
2022-03-09 15:11:01 +0100

    appointements

--- src/screens/Categories/CategoriesScreen.js
+++ src/screens/Categories/CategoriesScreen.js
@@ -9,7 +9,7 @@ export default function HomeScreen(props) {
   const { navigation } = props;
   const [doctor, setdoctors] = useState([]);
   useEffect(() => {
-    var ip= "http://192.168.1.191:3000"
+    var ip= "http://192.168.86.37:3000"
   axios.get(`${ip}/doctor/doctors`).then(res=>{
    setdoctors(res.data)
   }).catch(err=>{console.log(err);})
@@ -33,11 +33,12 @@ export default function HomeScreen(props) {
   };
 
   const renderRecipes = ({ item }) => (
-    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressRecipe(item)} >
+    <TouchableHighlight underlayColor="#fff" onPress={() => onPressRecipe(item)} >
       <View style={styles.container}>
         <Image style={styles.photo} source={{ uri: item.profilePicture }} />
         <Text style={styles.title}>{item.firstName}</Text>
         <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+        
       </View>
     </TouchableHighlight>
   );
